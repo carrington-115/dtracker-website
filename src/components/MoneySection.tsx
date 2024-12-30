7b515c41307dc86575787cc7b7a10c8f0ee0b028
carrington-115 <frumarkcarrington@gmail.com>
2024-12-31 02:39:36 +0530

    updated the structure of themoney section and css styles to fit onmobile

--- src/components/MoneySection.tsx
+++ src/components/MoneySection.tsx
@@ -12,7 +12,6 @@ export default function componentName() {
       <Container className="money-section">
         <h1
           style={{
-            ...textFontStyles.display.displayMediumBold,
             fontWeight: "bold",
           }}
         >
@@ -36,11 +35,28 @@ const Container = styled.section`
   margin-top: 50px;
   gap: 30px;
   width: 100%;
+  h1 {
+    font-size: 45px;
+  }
   .inner-container {
     width: 80%;
     display: flex;
     justify-content: space-between;
     gap: 30px;
     align-items: center;
+  }
+
+  @media (min-width: 320px) and (max-width: 599px) {
+    h1 {
+      font-size: 32px;
+      text-align: center;
+    }
+    .inner-container {
+      display: grid;
+      grid-template-columns: repeat(2, 1fr);
+      width: 90%;
+      row-gap: 20px;
+      column-gap: auto;
+    }
   }
 `;
