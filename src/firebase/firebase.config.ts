676a75ab23dadaf190b17f1d7f3d726de5518392
carrington-115 <frumarkcarrington@gmail.com>
2025-01-03 17:17:13 +0530

    installed firebase cli and added the firebase configuration files

---
+++ src/firebase/firebase.config.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA5TddTUBC7JQi1Wv6IQTyiZuJbbp7tCKQ",
+  authDomain: "dtracker-df0cc.firebaseapp.com",
+  projectId: "dtracker-df0cc",
+  storageBucket: "dtracker-df0cc.appspot.com",
+  messagingSenderId: "58249491372",
+  appId: "1:58249491372:web:09a4ef79932629488b8009",
+  measurementId: "G-E8N3EK6NL1",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
